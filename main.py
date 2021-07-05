2bbd5a427efa8e599323c2c2b58466d72dfa8994
lzh10382002 <77284145+lzh10382002@users.noreply.github.com>
2021-07-05 11:35:22 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,142 @@
+from model import *
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+#import tensorflow.compat.v1 as tf
+import tensorflow as tf
+
+from keras.backend.tensorflow_backend import set_session
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # select specific GPU based on your environment
+# prevent overuse of GPU memory
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+
+def adjustData(img,mask,flag_multi_class,num_class):
+    if(flag_multi_class):
+        img = img / 255
+        mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
+        new_mask = np.zeros(mask.shape + (num_class,))
+        for i in range(num_class):
+
+            new_mask[mask == i,i] = 1
+        new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
+        mask = new_mask
+    elif(np.max(img) > 1):
+        img = img / 255
+        mask = mask /255
+        mask[mask > 0.5] = 1
+        mask[mask <= 0.5] = 0
+    return (img,mask)
+
+
+def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "rgb",
+                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (512,512),seed = 1):#specification of training datasets
+
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+            target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    # return train_generator
+    for (img,mask) in train_generator:
+        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+        yield (img,mask)
+
+
+
+print("loading training data done")
+
+
+def vaildGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "rgb",
+                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (512,512),seed = 1): #specification of vaildation datasets
+
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+            target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    vaild_generator = zip(image_generator, mask_generator)
+    # return train_generator
+    for (img,mask) in vaild_generator:
+        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+        yield (img,mask)
+
+print("loading validation data done")
+
+data_gen_args = dict(rotation_range=20,
+                    width_shift_range=0.05,
+                    height_shift_range=0.05,
+                    shear_range=0.15,
+                    zoom_range=0.05,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
+
+
+myGene = trainGenerator(1, 'data/train', 'image', 'mask', data_gen_args, save_to_dir = None)
+myGene_vaild = vaildGenerator(1, 'data/test', 'image', 'mask', data_gen_args, save_to_dir = None)
+model_name = 'FCS_Net'
+#model_name = 'linknet.hdf5'
+print("got FCS_Net")
+model = FCS_Net()
+model_checkpoint = ModelCheckpoint(model_name+".hdf5", monitor='loss', verbose=1, save_best_only=True)
+
+print('Fitting model...')
+
+history = model.fit_generator(myGene, validation_data=myGene_vaild,validation_steps=2000,steps_per_epoch=2000, epochs=80, callbacks=[model_checkpoint])
+
+
+# save the training record
+with open(model_name+".txt", 'w') as f:
+    f.write(str(history.history))
+
+print('model saved ...{}'.format(model_name+".hdf5"))
+acc = history.history['acc']
+loss = history.history['loss']
+epochs = range(1, len(acc) + 1)
+# Draw the acc and loss
+plt.title('Accuracy and Loss')
+plt.plot(epochs, acc, 'red', label='Training acc')
+plt.plot(epochs, loss, 'blue', label='Validation loss')
+plt.legend()
+plt.show()
---
+++ model.py
@@ -0,0 +1,154 @@
+import numpy as np
+import os
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+#import tensorflow.compat.v1 as tf
+import tensorflow as tf
+from keras import metrics
+from keras.models import Model
+from keras.layers import Input
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.core import Activation, Dropout, Lambda
+from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import add, concatenate
+#from keras.optimizers import Adam
+from keras import backend as K
+import tensorflow as tf
+
+# dice loss defeind
+def dice_loss(smooth=1.):
+    def dice_loss_fixed(y_true, y_pred):
+        intersection = K.sum(y_true * y_pred)
+        union = K.sum(y_true)+K.sum(y_pred)
+        return 1. -(2.*intersection+smooth)/(union+smooth)
+    return dice_loss_fixed
+
+def conv_block(input_tensor, filters, strides, d_rates):
+    x = Conv2D(filters[0], kernel_size=1, dilation_rate=d_rates[0])(input_tensor)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters[1], kernel_size=3, strides=strides, padding='same', dilation_rate=d_rates[1])(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters[2], kernel_size=1, dilation_rate=d_rates[2])(x)
+    x = BatchNormalization()(x)
+
+    shortcut = Conv2D(filters[2], kernel_size=1, strides=strides)(input_tensor)
+    shortcut = BatchNormalization()(shortcut)
+
+    x = add([x, shortcut])
+    x = Activation('relu')(x)
+
+    return x
+
+
+def identity_block(input_tensor, filters, d_rates):
+    x = Conv2D(filters[0], kernel_size=1, dilation_rate=d_rates[0])(input_tensor)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters[1], kernel_size=3, padding='same', dilation_rate=d_rates[1])(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters[2], kernel_size=1, dilation_rate=d_rates[2])(x)
+    x = BatchNormalization()(x)
+
+    x = add([x, input_tensor])
+    x = Activation('relu')(x)
+
+    return x
+
+
+def pyramid_pooling_block(input_tensor, bin_sizes):
+    concat_list = [input_tensor]
+    h = input_tensor.shape[1].value
+    w = input_tensor.shape[2].value
+
+    for bin_size in bin_sizes:
+        x = AveragePooling2D(pool_size=(h//bin_size, w//bin_size), strides=(h//bin_size, w//bin_size))(input_tensor)
+        x = Conv2D(512, kernel_size=1)(x)
+        x = Lambda(lambda x: tf.image.resize_images(x, (h, w)))(x)
+
+        concat_list.append(x)
+
+    return concatenate(concat_list)
+
+
+def FCS_Net(pretrained_weights=None, input_size=(512, 512, 3)):
+    input = Input(input_size)
+
+    x = Conv2D(64, kernel_size=3, strides=(2, 2), padding='same')(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(64, kernel_size=3, strides=(1, 1), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(128, kernel_size=3, strides=(1, 1), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding="same")(x)
+
+    x = conv_block(x, filters=[64, 64, 256], strides=(1, 1), d_rates=[1, 1, 1])
+    x = identity_block(x, filters=[64, 64, 256], d_rates=[1, 1, 1])
+    x = identity_block(x, filters=[64, 64, 256], d_rates=[1, 1, 1])
+
+    x = conv_block(x, filters=[128, 128, 512], strides=(2, 2), d_rates=[1, 1, 1])
+    x = identity_block(x, filters=[128, 128, 512], d_rates=[1, 1, 1])
+    x = identity_block(x, filters=[128, 128, 512], d_rates=[1, 1, 1])
+    x = identity_block(x, filters=[128, 128, 512], d_rates=[1, 1, 1])
+
+    x = conv_block(x, filters=[256, 256, 1024], strides=(1, 1), d_rates=[1, 2, 1])
+    x = identity_block(x, filters=[256, 256, 1024], d_rates=[1, 2, 1])
+    x = identity_block(x, filters=[256, 256, 1024], d_rates=[1, 2, 1])
+    x = identity_block(x, filters=[256, 256, 1024], d_rates=[1, 2, 1])
+    x = identity_block(x, filters=[256, 256, 1024], d_rates=[1, 2, 1])
+    x = identity_block(x, filters=[256, 256, 1024], d_rates=[1, 2, 1])
+
+    x = conv_block(x, filters=[512, 512, 2048], strides=(1, 1), d_rates=[1, 4, 1])
+    x = identity_block(x, filters=[512, 512, 2048], d_rates=[1, 4, 1])
+    x = identity_block(x, filters=[512, 512, 2048], d_rates=[1, 4, 1])
+
+    x = pyramid_pooling_block(x, [1, 2, 3, 6])
+
+    x = Conv2D(512, kernel_size=3, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Dropout(0.1)(x)
+
+    x = Conv2D(2, kernel_size=1)(x)
+    x = Conv2DTranspose(2, kernel_size=(16, 16), strides=(8, 8), padding='same')(x)
+    x = Activation('softmax')(x)
+    model = Model(inputs=input, outputs=x)
+    #model = Model(img_input, x)
+    #model.compile(optimizer=Adam(lr=lr_init, decay=lr_decay),
+    #              loss='categorical_crossentropy',
+    #              metrics=[dice_coef])
+    model.compile(optimizer=Adam(lr=5e-6), loss=[dice_loss(smooth=1.)], metrics=['accuracy'])
+
+
+
+
+    model.summary()
+    if (pretrained_weights):
+        model.load_weights(pretrained_weights)
+        print('loaded pretrained_weights ... {}'.format(pretrained_weights))
+    return model
+
+
+
+
+
+
+
+
+
